0c84df2d05e3e392f8b82e161819575c27b27e5e
Ho Seok Song <hoseok@umich.edu>
2020-07-13 14:28:17 -0400

    adding more user authentication

--- flask_backend/blog_package/__init__.py
+++ flask_backend/blog_package/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +10,6 @@ app.config["SECRET_KEY"] = "5cfbe6997170e10d89fe47aeca5d27a05c059a8c5408d36a3d50
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blog_package import routes
--- flask_backend/blog_package/routes.py
+++ flask_backend/blog_package/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from blog_package import app, db, bcrypt
 from blog_package.forms import RegistrationForm, LoginForm
 from blog_package.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -45,14 +46,17 @@ posts = [
 
 @app.route('/')
 def renderHomePage():
-    return render_template("index.html", posts=posts, token="flask react")
+    return render_template("index.html", posts=posts, token="flask react", is_logged_in=current_user.is_authenticated)
 
 @app.route('/about')
 def renderAbout():
-    return render_template("about.html")
+    return render_template("about.html", is_logged_in=current_user.is_authenticated)
 
 @app.route("/register", methods=["GET", "POST"])
 def renderRegister():
+    if current_user.is_authenticated:
+        return redirect(url_for("renderHomePage"))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
@@ -65,11 +69,21 @@ def renderRegister():
 
 @app.route("/login", methods=["GET", "POST"])
 def renderLogin():
+    if current_user.is_authenticated:
+        return redirect(url_for("renderHomePage"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "password":
-            flash("You are logged in!", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for("renderHomePage"))
         else:
-            flash("Login unsuccessful. Please check username and/or password.", "danger")
+            flash("Login unsuccessful. Please check email and/or password.", "danger")
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def renderLogout():
+    logout_user()
+    return redirect(url_for("renderHomePage"))
+
