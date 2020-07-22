a1852812d9ba29fa6ad9302d025baf4f11f3c919
Ho Seok Song <hoseok@umich.edu>
2020-07-21 20:30:58 -0400

    configuring for deployment

--- flask_backend/blog_package/config.py
+++ flask_backend/blog_package/config.py
@@ -1,23 +1,14 @@
 import os
+import json
 
+with open("etc/annony_blog_config.json") as config_file
+    config = json.load(config_file)
 
 class Config:
-    cur_path = os.path.dirname(__file__)
-    secret_key_file = open(cur_path + "/private_variables/secret_key.txt", 'r')
-    SECRET_KEY = secret_key_file.read()
-    secret_key_file.close()
-
-    database_URI_file = open(cur_path + "/private_variables/database_URI.txt", 'r')
-    SQLALCHEMY_DATABASE_URI = database_URI_file.read()
-    database_URI_file.close()
-
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-
-    email_address_file = open(cur_path + "/private_variables/email_address.txt", 'r')
-    email_password_file = open(cur_path + "/private_variables/email_password.txt", 'r')
-    MAIL_USERNAME = email_address_file.read()
-    MAIL_PASSWORD = email_password_file.read()
-    email_address_file.close()
-    email_password_file.close()
+    MAIL_USERNAME = config.get("EMAIL_USER")
+    MAIL_PASSWORD = config.get("EMAIL_PASS")
