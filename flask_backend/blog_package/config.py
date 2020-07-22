7a9523e749385498bba35f9a5497b9b100e55a8e
Ho Seok Song <hoseok@umich.edu>
2020-07-21 20:33:17 -0400

    configuring for deployment

--- flask_backend/blog_package/config.py
+++ flask_backend/blog_package/config.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-with open("etc/annony_blog_config.json") as config_file
+with open("/etc/annony_blog_config.json") as config_file
     config = json.load(config_file)
 
 class Config:
